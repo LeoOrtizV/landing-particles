1709b8c1a517dcab0bf95d45287d876468892669
LeoOrtizV <leoyeret8126@gmail.com>
2025-05-15 18:58:56 -0600

    routes

--- components/circle-image.tsx
+++ components/circle-image.tsx
@@ -1,13 +1,13 @@
+// CircleImage.tsx
 "use client"
 
 import Image from 'next/image'
 
 const CircleImage = () => {
     return (
-        <div className="bottom-0 right-0 hidden md:inline-block md:absolute">
-            <Image src="/circles.png" width="300" height="300" className="w-full h-full " alt="Particles " />
+        <div className="pointer-events-none bottom-0 right-0 hidden md:inline-block md:absolute">
+            <Image src="/circles.png" width="300" height="300" className="w-full h-full" alt="Particles" />
         </div>
-
     );
 }
 
